63ae3081f8106c2a9240a6502e4498adb4577326
fuyutarow <fuyutarow@gmail.com>
2025-06-02 16:43:24 +0900

    refactor: update package.json and restructure battle market module
    - Simplified the "files" array in package.json for clarity.
    - Renamed the battle market export to "battleMarketLegacy" and added new module files for the legacy market functionalities.
    - Introduced constants, functions, and structures for the new market vault and token supply management.
    - Enhanced the init-loader to reflect the new module structure.

--- ts-sdk/src/suigen/_framework/init-loader.ts
+++ ts-sdk/src/suigen/_framework/init-loader.ts
@@ -1,7 +1,7 @@
 import * as package_onchain_1 from "../_dependencies/onchain/0x1/init";
 import * as package_onchain_2 from "../_dependencies/onchain/0x2/init";
 import * as package_onchain_f0f47c13aaa1b5ff53e4d44df713049d85515cf1c1884704b805d4523a23daec from "../_dependencies/onchain/0xf0f47c13aaa1b5ff53e4d44df713049d85515cf1c1884704b805d4523a23daec/init";
-import * as package_onchain_80ded5c56a6375c887fd4357487bd7d725712694b3b4b994d224b1ff23565364 from "../battleMarket/init";
+import * as package_onchain_80ded5c56a6375c887fd4357487bd7d725712694b3b4b994d224b1ff23565364 from "../battleMarketLegacy/init";
 import * as package_onchain_271c2fd30fd48ed9cf5b9bb903ccfbe19398becb2cab3c65026149a6a4a956b4 from "../champMarket/init";
 import * as package_onchain_1d58d7a49fafb509aef183464eaa4c5d1c2f26a56f4a7eb78ddbcd3c83713a38 from "../counter/init";
 import * as package_onchain_366ffbcaf9c51db02857ff81141702f114f3b5675dd960be74f3c5f34e2ba3c3 from "../justchat/init";
--- ts-sdk/src/suigen/index.ts
+++ ts-sdk/src/suigen/index.ts
@@ -2,5 +2,5 @@
 export * as counter from './Counter';
 export * as champMarket from './ChampMarket';
 export * as justchat from './Justchat';
-export * as battleMarket from './BattleMarket';
+export * as battleMarketLegacy from './BattleMarketLegacy';
 export * as mockcoins from './Mockcoins';
